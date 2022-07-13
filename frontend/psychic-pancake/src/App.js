95194c3b078e742f15535736bb86a875987e852a
Vasilis Poulopoulos <vasilispoulopoulos0@gmail.com>
2022-07-13 18:22:35 +0300

    admin navbar

--- frontend/psychic-pancake/src/App.js
+++ frontend/psychic-pancake/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/Landing';
 import AdminPanel from './pages/AdminPage';
+import AdminNavbar from './components/AdminNavbar';
 
 function App() {
+  console.log(process.env.REACT_APP_URL)
   return (
-    <div>
-      <Navbar />
-      <AdminPanel />
+    <div style={{backgroundColor:"#f0f0e1", width:"100%", height:"100%", minHeight:"100%"}}>
+      <AdminNavbar />
+      <div>
+        <AdminPanel />
+      </div>
     </div>
   );
 }
