8f0a759718e598ef28e7237f2a9684dda56fb8e0
Vasilis Poulopoulos <vasilispoulopoulos0@gmail.com>
2022-07-13 19:01:03 +0300

    started using react-router-dom

--- frontend/psychic-pancake/src/App.js
+++ frontend/psychic-pancake/src/App.js
@@ -4,16 +4,21 @@ import Navbar from './components/Navbar';
 import LandingPage from './pages/Landing';
 import AdminPanel from './pages/AdminPage';
 import AdminNavbar from './components/AdminNavbar';
-
+import { BrowserRouter } from "react-router-dom";
 function App() {
   console.log(process.env.REACT_APP_URL)
+  // const router = useRouter();
+  // console.log(router)
   return (
+    <BrowserRouter>
     <div style={{backgroundColor:"#f0f0e1", width:"100%", height:"100%", minHeight:"100%"}}>
       <AdminNavbar />
+      <span>Hello</span>
       <div>
-        <AdminPanel />
+        {/* <AdminPanel /> */}
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/psychic-pancake/src/components/AdminNavbar.js
+++ frontend/psychic-pancake/src/components/AdminNavbar.js
@@ -2,7 +2,6 @@ import { Link, Route, Router, Routes } from 'react-router-dom'
 
 export default function AdminNavbar() {
     return (
-        <Router>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Psychic Pancake</a>
@@ -11,30 +10,15 @@ export default function AdminNavbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                            <Routes>
-
-
                                 <li className="nav-item">
-                                    <Link to="/users">Users</Link>
+                                    <Link to={`/users`}>Users</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link to="/pending">Pending</Link>
+                                     <Link to="/pending">Pending</Link>
                                 </li>
-                            </Routes>
-
                         </ul>
                     </div>
                 </div>
             </nav>
-            <Routes>
-                <Route path={`/users`}>
-                    <AdminNavbar />
-                </Route>
-                <Route path={"/pending"}>
-                   <> </>
-                </Route>
-            </Routes>
-        </Router>
     );
 }
