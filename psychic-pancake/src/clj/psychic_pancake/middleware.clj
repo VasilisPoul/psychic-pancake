6ef0f490aa8065a80c13dd340d457387b420ad29
Thodoris Chatziioannidis <hthodoris@gmail.com>
2022-08-17 17:54:45 +0300

    cors

--- psychic-pancake/src/clj/psychic_pancake/middleware.clj
+++ psychic-pancake/src/clj/psychic_pancake/middleware.clj
@@ -3,8 +3,14 @@
     [psychic-pancake.env :refer [defaults]]
     [psychic-pancake.config :refer [env]]
     [ring-ttl-session.core :refer [ttl-memory-store]]
-    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
-    [ring.middleware.cors :refer [wrap-cors]]))
+    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+
+(defn my-wrap-cors [handler pat]
+  (fn [req]
+    (let [resp (handler req)]
+      (assoc-in resp
+              [:headers "Access-Control-Allow-Origin"]
+              pat))))
 
 (defn wrap-base [handler]
   (-> ((:middleware defaults) handler)
@@ -12,4 +18,4 @@
         (-> site-defaults
             (assoc-in [:security :anti-forgery] false)
             (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))
-      (wrap-cors :access-control-allow-origin #".*")))
+      (my-wrap-cors "*")))
