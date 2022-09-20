58771a35ba34177a33a140208ad0ba5ebd2e77a4
Vasilis Poulopoulos <vasilispoulopoulos0@gmail.com>
2022-09-20 22:10:36 +0300

    [frontend]: fixed maps

--- frontend/psychic-pancake/src/pages/Auction.js
+++ frontend/psychic-pancake/src/pages/Auction.js
@@ -8,7 +8,9 @@ import SendMessageModal from "../components/SendMessageModal";
 import AddBidModal from "../components/AddBidModal";
 import { useParams } from "react-router-dom";
 import axios from "../api/axios";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import '../style.css'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
 export default function Auction() {
   const { user, setUser } = useContext(UserContext);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,7 +20,7 @@ export default function Auction() {
   const [images, setImages] = useState([]);
   let images_array = [];
   useEffect(() => {
-    
+
     axios.get(`/api/listings/${auctionId}`)
       .then(
         function (response) {
@@ -41,10 +43,10 @@ export default function Auction() {
 
   return (
     <>
-      <head><link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-   integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-   crossorigin=""/><link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
-      </head>
+      {/* <head><link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+        crossorigin="" /><link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+      </head> */}
       {!loading &&
         <div>
           {(localStorage.getItem('AuthToken')) ? <Navbar /> : <InitialNavbar />}
@@ -82,13 +84,18 @@ export default function Auction() {
                   <h4>Description:</h4>
                   <span> {listing.description}</span>
                 </div>
-                <div id="map" style={{width: "80%", height: "100vh", zIndex: '1'}}>
-                  {/* <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
-                    <TileLayer
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                  </MapContainer> */}
+                <div id="map mt-4" style={{width: "80%", height: "100vh", zIndex: '1'}}>
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                  </Marker>
+                </MapContainer>
                 </div>
               </div>
             </div>
