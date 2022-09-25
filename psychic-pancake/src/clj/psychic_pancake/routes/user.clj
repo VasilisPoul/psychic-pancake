c83fd3aba390b9c17bada15c8e95d148cb876ca9
Thodoris Chatziioannidis <hthodoris@gmail.com>
2022-09-25 21:04:18 +0300

    [BACKEND] fixed 404 on GET /api/user/:id

--- psychic-pancake/src/clj/psychic_pancake/routes/user.clj
+++ psychic-pancake/src/clj/psychic_pancake/routes/user.clj
@@ -27,24 +27,24 @@
   ["/user"
    {:swagger {:tags ["user"]}}
    [""
-     {:post
-      {:parameters {:body specs.user/user-registration-shape}
-       :responses {200 {:body {:user :usr/ref}}
-                   409 {:body {:reason string?}}}
-       :handler (fn [{{params :body} :parameters}]
-                  (try
-                    (ok
-                     {:user
-                      (-> params
-                          (update :password h/derive)
-                          (rename-keys
-                           {:password :password_digest})
-                          (assoc :pending true)
-                          orm.user/create!)})
-                    (catch EntityExistsException e
-                      (conflict
-                       {:reason
-                        "A user with this id already exists"}))))}}]
+    {:post
+     {:parameters {:body specs.user/user-registration-shape}
+      :responses {200 {:body {:user :usr/ref}}
+                  409 {:body {:reason string?}}}
+      :handler (fn [{{params :body} :parameters}]
+                 (try
+                   (ok
+                    {:user
+                     (-> params
+                         (update :password h/derive)
+                         (rename-keys
+                          {:password :password_digest})
+                         (assoc :pending true)
+                         orm.user/create!)})
+                   (catch EntityExistsException e
+                     (conflict
+                      {:reason
+                       "A user with this id already exists"}))))}}]
    ["/me"
     {:swagger {:security [{:apiAuth []}]}
      :conflicting true
@@ -76,18 +76,19 @@
                :req->id (comp :id :path :parameters)
                :type :user}]
      :parameters {:path {:id :usr/uid}}
-     :conflicting true
-     :get
-     {:parameters {}
-      :responses {200 {:body {:role :usr/role
-                              :self :usr/ref
-                              :uid :usr/uid
-                              :first_name :usr/first_name
-                              :last_name :usr/last_name}}}
-      :handler user-get-handler}}
+     :conflicting true}
+     [""
+      {:get
+       {:parameters {}
+        :responses {200 {:body {:role :usr/role
+                                :self :usr/ref
+                                :uid :usr/uid
+                                :first_name :usr/first_name
+                                :last_name :usr/last_name}}}
+        :handler user-get-handler}}]
     ["/rating"
      {:put
-      {:parameters {:body {:rate #{1 2 3 4 5}}}
+      {:parameters {:body {:rate (s/and pos-int? #(< % 6))}}
        :responses {200 {:body {:user :usr/ref
                                :new-rating pos-int?}}}
        :fetch! [{:key :me
@@ -115,3 +116,4 @@
               (Rating. rating-id rate))
              (ok {:new-rating new-rating
                   :user user}))))}}]]])
+
